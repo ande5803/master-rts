7a06a026d5e158b3efbc42122eeb15e5b2ebdcfd
Anders Bundgaard <abund14@student.sdu.dk>
2019-01-06 14:16:24 +0100

    Draw 4 factory ships

--- core/src/com/sdu/abund14/master/paxbrit/PaxBritannicaCaseStudy.java
+++ core/src/com/sdu/abund14/master/paxbrit/PaxBritannicaCaseStudy.java
@@ -8,26 +8,38 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PaxBritannicaCaseStudy extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture img;
+	private Texture p1factory;
+	private Texture p2factory;
+	private Texture p3factory;
+	private Texture p4factory;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture(Gdx.files.internal("badlogic.jpg"));
+		p1factory = new Texture(Gdx.files.internal("factoryp1.png"));
+		p2factory = new Texture(Gdx.files.internal("factoryp2.png"));
+		p3factory = new Texture(Gdx.files.internal("factoryp3.png"));
+		p4factory = new Texture(Gdx.files.internal("factoryp4.png"));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(.3f, .6f, .7f, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(p1factory, 50, 50);
+		batch.draw(p2factory, 50, 300);
+		batch.draw(p3factory, 300, 300);
+		batch.draw(p4factory, 300, 50);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		p1factory.dispose();
+		p2factory.dispose();
+		p3factory.dispose();
+		p4factory.dispose();
 	}
 }
