fab3a5b0c28905184d5044bf202686392378462c
Anders Bundgaard <abund14@student.sdu.dk>
2019-03-20 16:11:54 +0100

    Another layer of multithreading on CombatShip and Collision processing, using fixed thread pools of 10 threads

--- core/src/com/sdu/abund14/master/paxbrit/processor/CollisionProcessor.java
+++ core/src/com/sdu/abund14/master/paxbrit/processor/CollisionProcessor.java
@@ -6,23 +6,43 @@ import com.sdu.abund14.master.paxbrit.bullet.Bullet;
 import com.sdu.abund14.master.paxbrit.interfaces.Processor;
 import com.sdu.abund14.master.paxbrit.ship.Ship;
 
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class CollisionProcessor implements Processor {
 
     private Match match = PaxBritannicaGame.currentMatch;
+    private ExecutorService threadPool = Executors.newFixedThreadPool(10);
+    private ExecutorCompletionService<Runnable> ecs = new ExecutorCompletionService<Runnable>(threadPool);
 
     @Override
     public void process(float delta) {
-        for (Bullet bullet : match.getBullets()) {
-            for (Ship ship : match.getAllShips()) {
-                if (bullet.getPlayerNumber() == ship.getPlayerNumber()) {
-                    continue; //Skip friendly fire collisions
+        int tasks = 0;
+        for (final Bullet bullet : match.getBullets()) {
+            tasks++;
+            ecs.submit(new Runnable() {
+                @Override
+                public void run() {
+                    for (Ship ship : match.getAllShips()) {
+                        if (bullet.getPlayerNumber() == ship.getPlayerNumber()) {
+                            continue; //Skip friendly fire collisions
+                        }
+                        //TODO Use something more precise than bounding rectangles
+                        if (bullet.getBoundingRectangle().overlaps(ship.getBoundingRectangle())) {
+                            ship.takeDamage(bullet.getDamage());
+                            bullet.destroy();
+                        }
+                    }
                 }
-                //TODO Use something more precise than bounding rectangles
-                if (bullet.getBoundingRectangle().overlaps(ship.getBoundingRectangle())) {
-                    ship.takeDamage(bullet.getDamage());
-                    bullet.destroy();
-                }
+            }, null);
+        }
+        try {
+            for (int i = 0; i < tasks; i++) {
+                ecs.take();
             }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
--- core/src/com/sdu/abund14/master/paxbrit/processor/CombatShipProcessor.java
+++ core/src/com/sdu/abund14/master/paxbrit/processor/CombatShipProcessor.java
@@ -9,23 +9,44 @@ import com.sdu.abund14.master.paxbrit.ship.Frigate;
 
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CombatShipProcessor implements Processor {
+
+    private ExecutorService threadPool = Executors.newFixedThreadPool(10);
+    private ExecutorCompletionService<Runnable> ecs = new ExecutorCompletionService<Runnable>(threadPool);
+
     @Override
-    public void process(float delta) {
+    public void process(final float delta) {
         removeDeadShips();
-        for (CombatShip ship : PaxBritannicaGame.currentMatch.getCombatShips()) {
-            ship.ai.update(delta);
-            if (ship.shotCooldownTimeLeft > 0) {
-                ship.shotCooldownTimeLeft -= delta;
-            } else {
-                ship.shotCooldownTimeLeft = 0;
+        int tasks = 0;
+        for (final CombatShip ship : PaxBritannicaGame.currentMatch.getCombatShips()) {
+            tasks++;
+            ecs.submit(new Runnable() {
+                @Override
+                public void run() {
+                    ship.ai.update(delta);
+                    if (ship.shotCooldownTimeLeft > 0) {
+                        ship.shotCooldownTimeLeft -= delta;
+                    } else {
+                        ship.shotCooldownTimeLeft = 0;
+                    }
+                    if (ship.reloadTimeLeft > 0) {
+                        ship.reloadTimeLeft -= delta;
+                    } else if (ship.ammoLeft <= 0) {
+                        ship.reload();
+                    }
+                }
+            }, null);
+        }
+        try {
+            for (int i = 0; i < tasks; i++) {
+                ecs.take();
             }
-            if (ship.reloadTimeLeft > 0) {
-                ship.reloadTimeLeft -= delta;
-            } else if (ship.ammoLeft <= 0) {
-                ship.reload();
-            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
         }
     }
 
