a30c88a2f0c1a354cad971482a67475debb2b9a3
Anders Bundgaard <abund14@student.sdu.dk>
2019-03-28 13:07:25 +0100

    GC Rev. 1 - Fix List allocation in CollisionProcessor

--- core/src/com/sdu/abund14/master/paxbrit/processor/CollisionProcessor.java
+++ core/src/com/sdu/abund14/master/paxbrit/processor/CollisionProcessor.java
@@ -1,6 +1,5 @@
 package com.sdu.abund14.master.paxbrit.processor;
 
-import com.sdu.abund14.master.paxbrit.Match;
 import com.sdu.abund14.master.paxbrit.PaxBritannicaGame;
 import com.sdu.abund14.master.paxbrit.bullet.Bullet;
 import com.sdu.abund14.master.paxbrit.interfaces.Processor;
@@ -8,13 +7,10 @@ import com.sdu.abund14.master.paxbrit.ship.Ship;
 
 public class CollisionProcessor implements Processor {
 
-    private Match match = PaxBritannicaGame.currentMatch;
-
     @Override
     public void process(float delta) {
-        for (int i = 0; i < match.getBullets().size(); i++) {
-            Bullet bullet = match.getBullets().get(i);
-            for (Ship ship : match.getAllShips()) {
+        for (Bullet bullet : PaxBritannicaGame.currentMatch.getBullets()) {
+            for (Ship ship : PaxBritannicaGame.currentMatch.getAllShips()) {
                 if (bullet.getPlayerNumber() == ship.getPlayerNumber()) {
                     continue; //Skip friendly fire collisions
                 }
