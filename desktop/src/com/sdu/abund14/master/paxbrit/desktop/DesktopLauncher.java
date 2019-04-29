6cc40edbf02fded08f8191848c11d3226c6bddf7
Anders Bundgaard <abund14@student.sdu.dk>
2019-04-29 16:09:39 +0200

    Increae FPS limit to 60

--- desktop/src/com/sdu/abund14/master/paxbrit/desktop/DesktopLauncher.java
+++ desktop/src/com/sdu/abund14/master/paxbrit/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 800;
 		config.height = 600;
-		config.foregroundFPS = 30;
+		config.foregroundFPS = 60;
 		new LwjglApplication(new PaxBritannicaGame(), config);
 	}
 }
